1c6428bf2f06f79652a6db443b843736beb21e84
dehovemarion30-a11y <dehovemarion3.0@gmail.com>
2026-03-13 15:22:18 +0100

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,20 @@
+#affichage du message de bienvenue
+print("Bienvenue dans notre restaurant!")
+print("Pour un hamburger, tapez 1.")
+print("Pour une pizza, tapez 2.")
+print("Pour une salade, tapez 3.")
+
+choix=input("Votre choix :")
+choix=int(choix)
+
+while choix not in [1,2,3]:
+    print("Veuillez répondre par 1, 2 ou 3.")
+    choix=input("Votre choix :")
+    choix=int(choix)
+
+if choix==1:
+    print("Vous avez choisi : hamburger")
+elif choix==2:
+    print("Vous avez choisi : pizza")
+elif choix==3:
+    print("vous avez choisi : salade")
